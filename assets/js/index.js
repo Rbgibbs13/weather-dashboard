623bc58c734a786d2b5441bb199933e85c082bc8
unknown <rhett.gibbs@yahoo.com>
2024-01-09 20:44:32 -0800

    Finished 5 day forecast - added styling to new elements - removed whitespace and console logs

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,16 @@
 let locationEl = document.querySelector("#location");
 let forecastEl = document.querySelector("#forecast");
 let inputEl = document.querySelector("#search-input");
+let switchEl = document.querySelector(".switch");
 let submitBtnEl = document.querySelector("#search-btn");
 let locationSearchParentEl = document.querySelector(".location-search");
 
+let holdWeatherData;
+let holdForecastData;
+var stateName;
+
 let savedSearches = 0;
+let useMetric = false;
 
 let monthsOfYear = {
     0: "January",
@@ -38,8 +44,7 @@ const FetchData = () => {
     }).then(function(response) {
         return response.json();
     }).then(function(data) {
-        console.log(data[0]);
-        var stateName = data[0].state;
+        stateName = data[0].state;
     
         //Build a url with geo data and fetch weather data with it
         var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + data[0].lat + "&lon=" + data[0].lon
@@ -51,6 +56,7 @@ const FetchData = () => {
             return response.json();
         }).then(function(data) {
             //Daily forecast data
+            holdWeatherData = data;
             GenerateDayForecast(data, stateName);
         });
     
@@ -63,14 +69,14 @@ const FetchData = () => {
             return response.json();
         }).then(function(data) {
             //5 day 3 hour forecast data
-            console.log(data.list[0].dt_txt);
-            ConvertUnix(data.list[0].dt);
+            holdForecastData = data.list;
+            GenerateFiveDayForecast(data.list);
+            //ConvertUnix(data.list[0].dt);
         });
     });
 }
 
 const GenerateDayForecast = (weatherData, state) => {
-    console.log(weatherData);
     //Check for existing data and remove
     var check = document.querySelector(".dataDiv");
     if(check) { check.remove(); }
@@ -89,7 +95,11 @@ const GenerateDayForecast = (weatherData, state) => {
 
     divEl.classList.add("dataDiv");
     spanEl.classList.add("material-symbols-outlined");
-    headEl.textContent = searchParams.cityName + ", " + state + " (" + ConvertUnix(weatherData.dt) +") ";
+    if(state === undefined) {
+        headEl.textContent = searchParams.cityName + " (" + ConvertUnix(weatherData.dt) +")";
+    } else {
+        headEl.textContent = searchParams.cityName + ", " + state + " (" + ConvertUnix(weatherData.dt) +")";
+    }
 
     if(weatherData.clouds.all < 25) {
         spanEl.textContent = "clear_day";
@@ -99,12 +109,18 @@ const GenerateDayForecast = (weatherData, state) => {
     }  else if(weatherData.clouds.all > 50) {
         //very cloudy
         spanEl.textContent = "cloud";
-        console.log("VERY CLOUDY");
+    }
+    
+    if(useMetric) {
+        tempEl.textContent = "Temp: " + ConvertKtoC(weatherData.main.temp) + "°C";
+        windEl.textContent = "Wind Speed: " + Math.floor(weatherData.wind.speed * 16)/10 + " KPH";
+    } else {
+        tempEl.textContent = "Temp: " + ConvertKtoF(weatherData.main.temp) + "°F";
+        windEl.textContent = "Wind Speed: " + Math.floor(weatherData.wind.speed * 10)/10 + " MPH";
     }
     
     cloudEl.textContent = "Cloud Cover: " + weatherData.clouds.all + "% ";
-    tempEl.textContent = "Temp: " + ConvertKtoF(weatherData.main.temp) + "°F";
-    windEl.textContent = "Wind Speed: " + Math.floor(weatherData.wind.speed * 10)/10 + " MPH";
+    
     windDirectionEl.textContent = "Wind Direction: " + ConvertWindDirection(weatherData.wind.deg);
     humidityEl.textContent = "Humidity: " + weatherData.main.humidity + "%";
     pressureEl.textContent = "Pressure: " + (weatherData.main.pressure/1000) + " bar";
@@ -112,24 +128,92 @@ const GenerateDayForecast = (weatherData, state) => {
     divEl.append(headEl);
     divEl.append(listParent);
     cloudEl.append(spanEl);
+    listParent.append(cloudEl);
     listParent.append(tempEl);
     listParent.append(windEl);
     listParent.append(windDirectionEl);
     listParent.append(humidityEl);
     listParent.append(pressureEl);
-    listParent.append(cloudEl);
 
     locationEl.append(divEl);
     StorePreviousSearch();
 }
 
 const GenerateFiveDayForecast = (weatherData) => {
+    //Check for existing data and remove
+    var check = document.querySelector(".forecast-parent-div");
+    if(check) { check.remove(); }
 
+    var parentDivEl = document.createElement("div");
+    var forecastDivEl = document.createElement("div");
+    var titleEl = document.createElement("h2");
+    var breakLineEl = document.createElement("hr");
+    var segmentDivEl;
+
+    parentDivEl.classList.add("forecast-parent-div");
+    forecastDivEl.classList.add("forecast-container");
+    breakLineEl.classList.add("horizontal-rule");
+    titleEl.textContent = "Five Day Forecast - 3 Hour Segments";
+
+    parentDivEl.append(titleEl);
+    parentDivEl.append(breakLineEl);
+    parentDivEl.append(forecastDivEl);
+
+    for(let i = 0; i < weatherData.length; i++) {
+        segmentDivEl = document.createElement("div");
+        segmentDivEl.classList.add("forecast-segment-div");
+
+        var dateTimeEl = document.createElement("h2");
+        var listParentEl = document.createElement("ul");
+        var tempEl = document.createElement("li");
+        var windEl = document.createElement("li");
+        var humidityEl = document.createElement("li");
+        var pressureEl = document.createElement("li");
+        var cloudEl = document.createElement("li");
+        var spanEl = document.createElement("span");
+
+        spanEl.classList.add("material-symbols-outlined");
+        dateTimeEl.textContent = ConvertUnix(weatherData[i].dt);
+        cloudEl.textContent = "Cloud Cover: " + weatherData[i].clouds.all + "%";
+        humidityEl.textContent = "Humidity: " + weatherData[i].main.humidity + "%";
+        pressureEl.textContent = "Pressure: " + (weatherData[i].main.pressure/1000) + " bar";
+
+        if(weatherData[i].clouds.all < 25) {
+            spanEl.textContent = "clear_day";
+        } else if(weatherData[i].clouds.all >= 25 && weatherData[i].clouds.all < 50) {
+            //partly clouds
+            spanEl.textContent = "partly_cloudy_day";
+        }  else if(weatherData[i].clouds.all > 50) {
+            //very cloudy
+            spanEl.textContent = "cloud";
+        }
+
+        if(useMetric) {
+            tempEl.textContent = "Temp: " + ConvertKtoC(weatherData[i].main.temp) + "°C";
+            windEl.textContent = "Wind Speed: " + Math.floor(weatherData[i].wind.speed * 16)/10 + " KPH";
+        } else {
+            tempEl.textContent = "Temp: " + ConvertKtoF(weatherData[i].main.temp) + "°F";
+            windEl.textContent = "Wind Speed: " + Math.floor(weatherData[i].wind.speed * 10)/10 + " MPH";
+        }
+
+        cloudEl.append(spanEl);
+        listParentEl.append(cloudEl);
+        listParentEl.append(tempEl);
+        listParentEl.append(windEl);
+        listParentEl.append(humidityEl);
+        listParentEl.append(pressureEl);
+
+        segmentDivEl.append(dateTimeEl);
+        segmentDivEl.append(listParentEl);
+        forecastDivEl.append(segmentDivEl);
+    }
+    
+    forecastEl.append(parentDivEl);
 }
 
 const GenerateLocalStorageList = () => {
     savedSearches = localStorage.length;
-    var maxSearches = 5;
+    var maxSearches = 7;
 
     for(let i = 0; i < localStorage.length; i++) {
         if(i > maxSearches) { return; }
@@ -180,19 +264,13 @@ const StorePreviousSearch = () => {
 }
 
 const RemoveFromStorage = (index) => {
-    console.log(index);
     var localLength = localStorage.length;
-    //localStorage.removeItem(index);
-    //index++;
 
     //Move everything in storage up one index
     if(index < localLength) {
         for(let i = index; i < localLength; i++) {
             var holdVal = localStorage.getItem(i);
-            console.log(holdVal);
-            if(i > 0) {
-                localStorage.setItem(i-1, holdVal);
-            }
+            if(i > 0) { localStorage.setItem(i-1, holdVal); }
             localStorage.removeItem(i);
         }
     }
@@ -218,10 +296,10 @@ const ConvertWindDirection = (windDir) => {
         //315-360 FROM north
         build += "S";
     }
-
     return build;
 }
 
+//Data converts Kelvin => Celsius  &&  Kelvin => Fahrenheit
 const ConvertKtoC = (kelvin) => {
     var celsius = Math.floor((kelvin - 273.15)*10)/10;
     return celsius;
@@ -266,13 +344,13 @@ const ConvertUnix = (unixTime) => {
         minutes = "0" + minutes;
     }
 
-    console.log(date.getMonth());
     var formatDate = " " + monthsOfYear[month] + "/" + day + "/" + year;
     var formatTime = hours + ":" + minutes + " " + state + " ";
     return formatTime + formatDate;
 }
 
-
+//Make sure switch always starts OFF
+switchEl.checked = false;
 GenerateLocalStorageList();
 submitBtnEl.addEventListener("click", function(event) {
     HandleUserInput();
@@ -281,4 +359,9 @@ inputEl.addEventListener("keypress", function(key) {
     if(key.key == "Enter") {
         HandleUserInput();
     }
+});
+switchEl.addEventListener("change", function() {
+    useMetric = !useMetric;
+    GenerateDayForecast(holdWeatherData, stateName);
+    GenerateFiveDayForecast(holdForecastData);
 });
